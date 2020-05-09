236ebf49a1128dbb06048e6653e3f492852d447e
Herbert Henrique <herbert.henrique@sumup.com>
2020-05-09 19:49:44 -0300

    feat: clean theme things and prepare for first launch

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,54 +1,57 @@
-import React from "react"
-import Helmet from "react-helmet"
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
 
-const ContactPage = ({
-  data: {
-    site
-  },
-}) => {
+const ContactPage = ({ data: { site } }) => {
   return (
     <Layout>
       <Helmet>
         <title>Contact — {site.siteMetadata.title}</title>
-        <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
+        <meta
+          name="description"
+          content={"Contact page of " + site.siteMetadata.description}
+        />
       </Helmet>
-      <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
-          <h1 className="post-title">Get in Touch</h1>
-          <p>Let me help you kick start your next project &rarr;</p>
-        </div>
-        <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
-            <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
-            </div>
-            <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
-            </div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
-            </div>
-          </form>
+      <div className="hero-header">
+        <div className="headline">get in touch</div>
+        <div className="primary-content">
+          <p>
+            you can find on{" "}
+            <a
+              href="https://twitter.com/herberth3nrique"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {" "}
+              twitter
+            </a>
+            ,{" "}
+            <a
+              href="https://github.com/herberthenrique"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              github
+            </a>{" "}
+            or{" "}
+            <a
+              href="https://instagram.com/_herberthenrique"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              instagram
+            </a>
+          </p>
+          or just send me a mail to contact@herberthenrique twitter mail 📝
         </div>
       </div>
     </Layout>
-  )
-}
-export default ContactPage
+  );
+};
+export default ContactPage;
 export const pageQuery = graphql`
-  query ContactPageQuery{
+  query ContactPageQuery {
     site {
       siteMetadata {
         title
@@ -56,4 +59,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
